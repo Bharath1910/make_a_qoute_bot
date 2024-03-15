47722baf0c3c6e0a05fe154609590730d12fcbf7
Bharath1910 <av.bharath@proton.me>
2024-03-15 16:20:43 +0530

    added handle subcommand handler to global client

--- src/index.ts
+++ src/index.ts
@@ -2,18 +2,20 @@ import { ActivityType, Client, Collection, GatewayIntentBits } from 'discord.js'
 import * as dotenv from 'dotenv'
 import { loadEvents } from './events/index'
 import type { CustomClient } from './typings'
+import { handleSubCommands } from './utils/handleSubcommands'
 
 async function main (): Promise<void> {
   dotenv.config()
-  const client: CustomClient = new Client({
+  const client = new Client({
     intents: [
       GatewayIntentBits.Guilds,
       GatewayIntentBits.GuildMessages,
       GatewayIntentBits.MessageContent
     ]
-  })
+  }) as CustomClient
 
   client.commands = new Collection()
+  client.handleSubcommands = handleSubCommands
 
   const { DISCORD_TOKEN } = process.env
 
