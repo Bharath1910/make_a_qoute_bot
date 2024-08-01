1a4db78ab867c8101f83113e40e41679ed48c2e9
Bharath1910 <av.bharath@proton.me>
2024-08-01 11:20:37 +0530

    testing

--- src/utils/make_qoute.ts
+++ src/utils/make_qoute.ts
@@ -1,16 +1,30 @@
 import Jimp from "jimp"
+import textWrapper from "./text_wrapper"
 
 export default async function makeQoute(
 	avatarURL: string,
-	qoute: string,
+	quote: string,
 	user: string
 ): Promise<Buffer> {
+	console.log(avatarURL, quote, user)
 	const image = await Jimp.read(avatarURL)
 	image.contain(1000, 450, Jimp.HORIZONTAL_ALIGN_LEFT)
 	image.background(0x000000)
 	image.grayscale()
+
 	const srcImage = await Jimp.read("src/assets/mask.png")
 	srcImage.resize(400, 450)
+
 	image.composite(srcImage, 50, 0)
+	const font = await Jimp.loadFont(Jimp.FONT_SANS_32_WHITE)
+	await textWrapper(
+		image, 
+		quote.split(" "), 
+		0, 
+		0, 
+		{x: 400, y: 0}
+	)
+
+
 	return Buffer.from(await image.getBufferAsync(Jimp.MIME_JPEG))
 }
